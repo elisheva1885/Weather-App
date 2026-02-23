d9eaf88eff0d4a79a82124e089c7a898ae4f85ff
elisheva1885 <elisheva1885@gmail.com>
2026-02-23 14:44:10 +0200

    fix the notes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_KEY = 'd9d23c4b33da4fb5bd3121603261702';
-let interval = 30000;
+const INTERVAL_TIMER = 30000;
 
 const getData = async (location) => {
     const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location.coords.latitude},${location.coords.longitude}`;
@@ -28,11 +28,11 @@ const getLocation = () => {
 }
 
 getLocation()
-setInterval(getLocation, interval)
+setInterval(getLocation, INTERVAL_TIMER)
 
 const showData = (data) => {
-    let degrees = Math.round(data.current.temp_c) 
-    let feelslike = Math.round(data.current.feelslike_c)
+    const degrees = Math.round(data.current.temp_c) 
+    const feelslike = Math.round(data.current.feelslike_c)
     document.getElementById("condImg").src = data.current.condition.icon;
     document.getElementById("degrees").textContent = degrees ;
     document.getElementById("condition").textContent = data.current.condition.text;
